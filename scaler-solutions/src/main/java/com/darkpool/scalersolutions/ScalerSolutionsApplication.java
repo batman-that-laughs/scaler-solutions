fa9181b0ca48f9092212e897dca18f410197b483
batman-that-laughs <b4tm4n.th4t.l4ughs@gmail.com>
2021-05-20 06:31:44 +0000

    Initial Commit

---
+++ scaler-solutions/src/main/java/com/darkpool/scalersolutions/ScalerSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.darkpool.scalersolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalerSolutionsApplication.class, args);
+	}
+
+}
---
+++ scaler-solutions/src/test/java/com/darkpool/scalersolutions/ScalerSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkpool.scalersolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
